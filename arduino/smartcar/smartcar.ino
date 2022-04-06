ff0173b960de9433809140ebb5faf17a3a046fa4
Quinstedt <90027419+Quinstedt@users.noreply.github.com>
2022-04-06 22:05:33 +0200

    Basic obstacle avoidance
    Simple Arduino sketch for basic obstacle avoidance assigment. The car will detect obstacles and stop, go backwards and continues in another direction with a small accelaration. The car will continue driving until it detects another obstacle.
    
    new version of pull request #10

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,80 @@
 #include <Smartcar.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int fSpeed   = 70;  
+const int bSpeed   = -70; 
+const int lDegrees = -75; 
+const int rDegrees = 75;  
+const int TRIGGER_PIN = 6;
+const int ECHO_PIN = 7;
+const unsigned int MAX_DISTANCE =100;
+
+ArduinoRuntime arduinoRuntime;
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SimpleCar car(control);
+
+void setup()
+{
+    Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void handleInput(){
+    if(Serial.available())
+    {
+        char input = Serial.read(); 
+                                    
+        switch (input)
+        {
+        case 'l': 
+            car.setSpeed(fSpeed);
+            car.setAngle(lDegrees);
+            break;
+        case 'r': 
+            car.setSpeed(fSpeed);
+            car.setAngle(90);
+            break;
+        case 'f': 
+            car.setSpeed(fSpeed);
+            car.setAngle(0);
+            break;
+        case 'b': 
+            car.setSpeed(bSpeed);
+            car.setAngle(0);
+            break;
+        default: 
+            car.setSpeed(0);
+            car.setAngle(0);
+        }
+    }
+}
+
+void loop()
+{
+   
+    Serial.println(front.getDistance());
+    delay(100);
+    
+    if(front.getDistance() <= 100 && front.getDistance()>0){
+          car.setSpeed(0);
+       // the car go back   
+          car.setSpeed(bSpeed);
+          delay(1500);
+       // change angle to the left   
+           car.setAngle(90);
+           delay(1000);
+           car.setAngle(0);
+      // code below to create a small accelaration     
+           car.setSpeed(20);
+           delay(1000);
+           car.setSpeed(40);
+           delay(1000);
+           car.setSpeed(fSpeed);
+           
+     }
+    
+     handleInput();
+      delay(1);
 }
