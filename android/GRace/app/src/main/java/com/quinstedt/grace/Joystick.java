cdf6ca9052915575f5277487a26b383e7982dd79
Quinstedt <gusquini@student.gu.se>
2022-04-21 11:57:21 +0200

    Joystick library test fix

--- android/GRace/app/src/main/java/com/quinstedt/grace/Joystick.java
+++ android/GRace/app/src/main/java/com/quinstedt/grace/Joystick.java
@@ -2,8 +2,10 @@ package com.quinstedt.grace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.joystickjhr.JoystickJhr;
+
 public class Joystick extends AppCompatActivity {
 
-    
+    final JoystickJhr joystick = findViewById(R.id.joystick);
 
 }
