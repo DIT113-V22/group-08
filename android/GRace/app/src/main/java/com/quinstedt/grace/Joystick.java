11e995825ff89ffeb3f6c9c32e04ebec675bca6e
Quinstedt <gusquini@student.gu.se>
2022-04-21 12:56:11 +0200

    changed to gradle wrapper validation CI

--- android/GRace/app/src/main/java/com/quinstedt/grace/Joystick.java
+++ android/GRace/app/src/main/java/com/quinstedt/grace/Joystick.java
@@ -2,10 +2,8 @@ package com.quinstedt.grace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.joystickjhr.JoystickJhr;
-
 public class Joystick extends AppCompatActivity {
 
-    final JoystickJhr joystick = findViewById(R.id.joystick);
+    
 
 }
