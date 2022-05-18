e55209cd27a8c5ede77f6f8ab6cb42fa7875a6d1
Quinstedt <gusquini@student.gu.se>
2022-05-18 12:55:30 +0200

    Fixed the test so that they match this version

--- Android/islandRush/app/src/androidTest/java/com/quinstedt/islandRush/LeaderboardTest.kt
+++ Android/islandRush/app/src/androidTest/java/com/quinstedt/islandRush/LeaderboardTest.kt
@@ -18,97 +18,96 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class LeaderboardTest {
 
+
     val wait = Thread.sleep(3000)
     /** NAVIGATION TESTS*/
     @Test
     fun test_Launching_LeaderboardActivity(){
-        val activityTest = ActivityScenario.launch(Leaderboard::class.java)
-        onView(withId(R.id.leaderboardMain)).check(matches(isDisplayed()))
-        onView(withId(R.id.leaderboard1)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard1::class.java)
+        onView(withId(R.id.Leaderboard1)).check(matches(isDisplayed()))
+        onView(withId(R.id.Leaderboard1)).check(matches(isDisplayed()))
         wait
     }
 
     @Test
     fun test_SwipeToLeaderboard2(){
-        val activityTest = ActivityScenario.launch(Leaderboard::class.java)
-        onView(withId(R.id.leaderboardMain)).perform(swipeLeft())
-        onView(withId(R.id.leaderboard2)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard1::class.java)
+        onView(withId(R.id.Leaderboard1)).perform(swipeLeft())
+        onView(withId(R.id.Leaderboard2)).check(matches(isDisplayed()))
         wait
 
     }
     @Test
     fun test_SwipeToLeaderboard1(){
-        val activityTest = ActivityScenario.launch(Leaderboard::class.java)
-        onView(withId(R.id.leaderboardMain)).perform(swipeRight())
-        onView(withId(R.id.leaderboard1)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard2::class.java)
+        onView(withId(R.id.Leaderboard2)).perform(swipeRight())
+        onView(withId(R.id.Leaderboard1)).check(matches(isDisplayed()))
         wait
     }
     @Test
     fun test_clickTab_ToLeaderboard2(){
-        val activityTest = ActivityScenario.launch(Leaderboard::class.java)
-        onView(withText("Leaderboard 2")).perform(click());
-        onView(withId(R.id.leaderboard2)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard1::class.java)
+        onView(withId(R.id.GoToLeaderboard2)).perform(click());
+        onView(withId(R.id.Leaderboard2)).check(matches(isDisplayed()))
         wait
     }
     @Test
     fun test_clickTab_ToLeaderboard1(){
-        val activityTest = ActivityScenario.launch(Leaderboard::class.java)
-        onView(withId(R.id.leaderboardMain)).perform(swipeLeft())
-        onView(withText("Leaderboard 1")).perform(click());
-        onView(withId(R.id.leaderboard1)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard2::class.java)
+        onView(withId(R.id.GoToLeaderboard1)).perform(click());
+        onView(withId(R.id.Leaderboard1)).check(matches(isDisplayed()))
         wait
     }
 
     /** LAYOUT TESTS*/ //  L2
     @Test
     fun test_leaderboard1_Ranking(){
-        lateinit var scenario: FragmentScenario<Leaderboard1>
-        scenario = launchFragmentInContainer(themeResId = R.style.Theme_IslandRush)
-        scenario.moveToState(Lifecycle.State.STARTED)
+        val activityTest = ActivityScenario.launch(Leaderboard1::class.java)
 
-        onView(withId(R.id.rank1)).check(matches(isDisplayed()))
-        onView(withId(R.id.rank2)).check(matches(isDisplayed()))
-        onView(withId(R.id.rank3)).check(matches(isDisplayed()))
-        onView(withId(R.id.rank4)).check(matches(isDisplayed()))
-        onView(withId(R.id.rank5)).check(matches(isDisplayed()))
+        onView(withId(R.id.Rank1)).check(matches(isDisplayed()))
+        onView(withId(R.id.Rank2)).check(matches(isDisplayed()))
+        onView(withId(R.id.Rank3)).check(matches(isDisplayed()))
+        onView(withId(R.id.Rank4)).check(matches(isDisplayed()))
+        onView(withId(R.id.Rank5)).check(matches(isDisplayed()))
         wait
     }
     @Test
     fun test_leaderboard2_Ranking(){
-        lateinit var scenario: FragmentScenario<Leaderboard2>
-        scenario = launchFragmentInContainer(themeResId = R.style.Theme_IslandRush)
-        scenario.moveToState(Lifecycle.State.STARTED)
-        onView(withId(R.id.L2rank1)).check(matches(isDisplayed()))
-        onView(withId(R.id.L2rank2)).check(matches(isDisplayed()))
-        onView(withId(R.id.L2rank3)).check(matches(isDisplayed()))
-        onView(withId(R.id.L2rank4)).check(matches(isDisplayed()))
-        onView(withId(R.id.L2rank5)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard2::class.java)
+
+        onView(withId(R.id.L2Rank1)).check(matches(isDisplayed()))
+        onView(withId(R.id.L2Rank2)).check(matches(isDisplayed()))
+        onView(withId(R.id.L2Rank3)).check(matches(isDisplayed()))
+        onView(withId(R.id.L2Rank4)).check(matches(isDisplayed()))
+        onView(withId(R.id.L2Rank5)).check(matches(isDisplayed()))
         wait
     }
     @Test
     fun test_leaderBoard1_MedalsImages(){
-        lateinit var scenario: FragmentScenario<Leaderboard1>
-        scenario = launchFragmentInContainer(themeResId = R.style.Theme_IslandRush)
-        scenario.moveToState(Lifecycle.State.STARTED)
-        onView(withId(R.id.gold_medal)).check(matches(isDisplayed()))
-        onView(withId(R.id.silver_medal)).check(matches(isDisplayed()))
-        onView(withId(R.id.bronze_medal)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard1::class.java)
+        onView(withId(R.id.Gold_medal)).check(matches(isDisplayed()))
+        onView(withId(R.id.Silver_medal)).check(matches(isDisplayed()))
+        onView(withId(R.id.Bronze_medal)).check(matches(isDisplayed()))
         wait
     }
     @Test
     fun test_leaderBoard2_MedalsImages(){
-        lateinit var scenario: FragmentScenario<Leaderboard2>
-        scenario = launchFragmentInContainer(themeResId = R.style.Theme_IslandRush)
-        scenario.moveToState(Lifecycle.State.STARTED)
-        onView(withId(R.id.L2gold_medal)).check(matches(isDisplayed()))
-        onView(withId(R.id.L2silver_medal)).check(matches(isDisplayed()))
-        onView(withId(R.id.L2bronze_medal)).check(matches(isDisplayed()))
+        val activityTest = ActivityScenario.launch(Leaderboard1::class.java)
+        onView(withId(R.id.L2Gold_medal)).check(matches(isDisplayed()))
+        onView(withId(R.id.L2Silver_medal)).check(matches(isDisplayed()))
+        onView(withId(R.id.L2Bronze_medal)).check(matches(isDisplayed()))
         wait
     }
     @Test
-    fun test_escapeHash(){
-        val activityTest = ActivityScenario.launch(Leaderboard::class.java)
-        onView(withId(R.id.leaderboard_escapeHash)).perform(click());
+    fun test_escapeHash_Leaderboard1(){
+        val activityTest = ActivityScenario.launch(Leaderboard1::class.java)
+        onView(withId(R.id.Leaderboard_escapeHash)).perform(click());
+        onView(withId(R.id.mainLayout)).check(matches(isDisplayed()))
+    }
+    @Test
+    fun test_escapeHash_Leaaderboard2(){
+        val activityTest = ActivityScenario.launch(Leaderboard2::class.java)
+        onView(withId(R.id.L2Leaderboard_escapeHash)).perform(click())
         onView(withId(R.id.mainLayout)).check(matches(isDisplayed()))
     }
 
