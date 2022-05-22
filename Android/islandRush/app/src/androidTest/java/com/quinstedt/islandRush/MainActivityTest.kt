baf3e6a5bd1a809561a21fb7483cd078417db3d1
Quinstedt <gusquini@student.gu.se>
2022-05-22 23:23:09 +0200

    commenting the suite Test class and the manual test

--- Android/islandRush/app/src/androidTest/java/com/quinstedt/islandRush/MainActivityTest.kt
+++ Android/islandRush/app/src/androidTest/java/com/quinstedt/islandRush/MainActivityTest.kt
@@ -5,6 +5,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -92,8 +93,8 @@ class MainActivityTest {
      *  NOTE: to avoid problems with the CI make sure to comment @Test */
 
 
-  // @LargeTest
-    @Test
+   @LargeTest
+  //  @Test
     fun test_process_of_entering_a_playerName(){
         val activityTest = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.playerName)).perform(click())
