0065c221fe777a9b1c4fabd78e8284f4eac81928
safayoussef <92092634+safayoussef@users.noreply.github.com>
2022-05-25 11:44:24 +0200

    Remerged with master branch
    Remerged with master branch because the previous pull request was outdated.
    
    Moved the credit button a little and changed the animation slightly.

--- Android/islandRush/app/src/main/java/com/quinstedt/islandRush/activityClasses/CreditsActivity.java
+++ Android/islandRush/app/src/main/java/com/quinstedt/islandRush/activityClasses/CreditsActivity.java
@@ -1,4 +1,4 @@
-package com.quinstedt.islandRush;
+package com.quinstedt.islandRush.activityClasses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+
+import com.quinstedt.islandRush.R;
 
 public class CreditsActivity extends AppCompatActivity {
 
