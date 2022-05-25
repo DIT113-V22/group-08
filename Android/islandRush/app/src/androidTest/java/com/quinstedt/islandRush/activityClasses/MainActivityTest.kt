155e7708befd383bd50c030e427ece85070d1d01
Quinstedt <gusquini@student.gu.se>
2022-05-25 21:41:03 +0200

    Fixed the speedometer bug while using the Reset button

--- Android/islandRush/app/src/androidTest/java/com/quinstedt/islandRush/activityClasses/MainActivityTest.kt
+++ Android/islandRush/app/src/androidTest/java/com/quinstedt/islandRush/activityClasses/MainActivityTest.kt
@@ -117,8 +117,8 @@ class MainActivityTest {
      *  NOTE: to avoid problems with the CI make sure to comment @Test */
 
 
-   @LargeTest
-  //  @Test
+  // @LargeTest
+    @Test
     fun test_process_of_entering_a_playerName(){
         val activityTest = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.playerName)).perform(click())
